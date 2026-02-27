1eb99ed54b41c57b75ce1f90b3e773a9599b1352
Augusto Luiz Marim <augustolm06@gmail.com>
2026-02-27 08:23:50 -0300

    Add files via upload

---
+++ Atividade1/Atividade1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Shapes;
+
+namespace Atividade1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string resposta;
+        private float valor;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonConverter_Click(object sender, RoutedEventArgs e)
+        {
+            valor = float.Parse(TextBoxValor.Text);
+            resposta = ((valor * 1.8) + 32).ToString();
+            LabelResposta.Content = $"{resposta} Graus Fahrenheit";
+        }
+    }
+}
